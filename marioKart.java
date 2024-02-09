14e1359ebda8231efc28c3dbc8d814677ba74569
Hans <htvanlierop@loyola.edu>
2024-02-08 23:34:37 -0500

    In the middle of creating user input for player values

--- marioKart.java
+++ marioKart.java
@@ -1,12 +1,58 @@
-// Read the README.md
-// Read it again
-// Your code here
-// Delete these 4 lines of comments
+import java.util.Scanner;
 
-class HelloWorld {
+class MarioKartPlayer {
+    //Attributes
+    String playerName, character;
+    int speed;
+
+    //General Constructor
+    public MarioKartPlayer(String playerName, String character, int speed) {
+        this.playerName = playerName;
+        this.character = character;
+        this.speed = speed;
+    }
+
+    public MarioKartPlayer() {
+        this.playerName = "unknown";
+        this.character = "unknown";
+        this.speed = 1;
+    }
+
+    // Boost method (increase speed)
+    public void boost() {
+        System.out.println(playerName + " performed a boost!\n");
+        speed++;
+    }
+
+    // Display info of player
+    public void displayInfo() {
+        System.out.println("Player Name: " + playerName + "\nCharacter: " + character + "\nSpeed: " + speed + "\n");
+    }
+}
+
+class MarioKartGame {
     public static void main(String[] args) {
+        System.out.println("Mario Kart!\n");
+        Scanner input = new Scanner(System.in);
+        // Prompt user input for player data
+        System.out.println("Enter your name: ");
+        String playerName = input.next();
+        System.out.println("Enter ")
+        String character = input.next();
+        int
+        // Instantiate two objects yoshi
+        MarioKartPlayer yoshi = new MarioKartPlayer();
 
-        System.out.println("Mario Kart!");
-        
+        playerName = input.next();
+        MarioKartPlayer bowser = new MarioKartPlayer();
+        // Display the information of both characters
+        yoshi.displayInfo();
+        bowser.displayInfo();
+        // Call the boost method on yoshi and display updated info
+        yoshi.boost();
+        yoshi.displayInfo();
+        // Call the boost method on bowser and display updated info
+        bowser.boost();
+        bowser.displayInfo();
     }
 }
